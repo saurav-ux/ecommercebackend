10c7f0252646772efc8a1bf6cee2f9b2876ff853
Saurav Anand <saurav.anand@auctopustechnologies.com>
2023-10-11 23:31:06 +0530

    cloud

--- Database/connection.js
+++ Database/connection.js
@@ -1,9 +1,17 @@
 import mongoose from "mongoose";
-mongoose.connect("mongodb://127.0.0.1:27017/sauravdatabase",{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(()=>{
-    console.log("Success")
-}).catch((e)=>{
-    console.log("Connection failed")
-})
+
+const dbName = "sauravdatabase";
+// const connect1 = `mongodb+srv://sauravanand243:Saurav@123@cluster0.jbbncif.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+const connect1 = 'mongodb+srv://sauravanand243:Ejse5gkM6dG0UECt@cluster0.9jeqyld.mongodb.net/?retryWrites=true&w=majority'
+
+mongoose.connect(connect1, {
+  // useNewUrlParser: true,
+  // useUnifiedTopology: true
+}).then(() => {
+  console.log("Connected to MongoDB");
+}).catch((error) => {
+  console.error("Connection failed:", error);
+});
+
+
+
